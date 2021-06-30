1156f634acce8e7daa3cdcaa0b11b18737be8e72
mohamed <mohamed6540@gmail.com>
2021-06-30 02:41:28 +0200

    a

--- lib/screens/dashboard/components/departments.dart
+++ lib/screens/dashboard/components/departments.dart
@@ -1,21 +1,19 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'dart:ui';
-// import 'package:admin/screens/main/components/side_menu.dart';
+import 'package:admin/responsive.dart';
+import 'package:admin/screens/main/components/side_menu.dart';
 
 class Departments extends StatefulWidget {
   static const String id = 'departments';
-
   State<StatefulWidget> createState() {
     return HomeState();
   }
 }
-
 class HomeState extends State<Departments> {
   @override
   Widget build(BuildContext context) {
- 
     return Scaffold(
-      //backgroundColor: Colors.white,
       appBar: AppBar(
         title: Text(
           "hospital system",
@@ -26,243 +24,36 @@ class HomeState extends State<Departments> {
           IconButton(
               icon: Icon(Icons.search),
               onPressed: () {
-               // showSearch(context: context, delegate: DataSearch());
               })
         ],
         centerTitle: true,
         elevation: 20,
-        //leading:IconButton(icon: Icon(Icons.access_alarm), onPressed: (){}),
-        //titleSpacing: 100,
-        // brightness: Brightness.dark,
-        //primary: false,
       ),
-
-      body: ListView(children: <Widget>[
-       /* Container(
-          height: 600,
-          width: double.infinity,
-          margin: EdgeInsets.all(20),
-          
-        ),
- 
-
+      drawer: SideMenu(),
+      body: SafeArea(
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            // We want this side menu only for large screen
+            if (Responsive.isDesktop(context))
+              Expanded(
+                child: SideMenu(),
+              ),
+            Expanded(
+              flex: 5,
+               child: ListView(children: <Widget>[
         Container(
-          height: 200,
-               
-          child: ListView(
-            scrollDirection: Axis.horizontal,
-            children: <Widget>[
-              Container(
-                height: 200,
-                width: 250,
-                child: ListTile(
-                  title: Image.asset('images/pharmacy.png',
-                      height: 170, width: 250, fit: BoxFit.fill),
-                  subtitle: Container(
-                    child: Text(
-                      'pharmacy',
-                      style:
-                          TextStyle(fontSize: 17, fontWeight: FontWeight.bold),
-                      textAlign: TextAlign.center,
-                      //style: TextStyle(color: Colors.black),
-                    ),
-                  ),
-                  onLongPress: () {
-                    print("long press");
-                  },
-                  onTap: () {
-                    print("tap");
-                  },
-                ),
-              ),
-              Container(
-                 height: 200,
-                width: 30,
-              ),
-
-              Container(
-               height: 200,
-                width: 250,
-                child: ListTile(
-                  title: Image.asset('images/Operating_theatre.jpg',
-                      height: 170, width: 250, fit: BoxFit.fill),
-                  subtitle: Container(
-                    child: Text(
-                      'Operating_theatre',
-                      style:
-                          TextStyle(fontSize: 14, fontWeight: FontWeight.bold),
-                      textAlign: TextAlign.center,
-                      // style: TextStyle(color: Colors.black)
-                    ),
-                  ),
-                  onLongPress: () {
-                    print("long press");
-                  },
-                  onTap: () {
-                    print("tap");
-                  },
-                ),
-              ),
-
-              Container(
-                 height: 200,
-                width: 30,
-              ),
-
-              Container(
-               height: 200,
-                width: 250,
-                child: ListTile(
-                  title: Image.asset('images/emergency.jpg',
-                    height: 170, width: 250, fit: BoxFit.fill),
-                  subtitle: Container(
-                    child: Text(
-                      'E_R',
-                      style:
-                          TextStyle(fontSize: 14, fontWeight: FontWeight.bold),
-                      textAlign: TextAlign.center,
-                      // style: TextStyle(color: Colors.black)
-                    ),
-                  ),
-                  onLongPress: () {
-                    print("long press");
-                  },
-                  onTap: () {
-                    print("tap");
-                  },
-                ),
-              ),
-              Container(
-               height: 200,
-                width: 30,
-              ),
-
-              Container(
-                height: 200,
-                width: 250,
-                child: ListTile(
-                  title: Image.asset('images/laboratory_2.png',
-                     height: 170, width: 250, fit: BoxFit.fill),
-                  subtitle: Container(
-                    child: Text(
-                      'laboratory',
-                      style:
-                          TextStyle(fontSize: 15, fontWeight: FontWeight.bold),
-                      textAlign: TextAlign.center,
-                      //style: TextStyle(color: Colors.black)
-                    ),
-                  ),
-                  onLongPress: () {
-                    print("long press");
-                  },
-                  onTap: () {
-                    print("tap");
-                  },
-                ),
-              ),
-              Container(
-              height: 200,
-                width: 30
-                ),
-
-              Container(
-                height: 200,
-                width: 250,
-                child: ListTile(
-                  title: Image.asset('images/opd.jpg',
-                     height: 170, width: 250, fit: BoxFit.fill),
-                  subtitle: Container(
-                    child: Text(
-                      'OPD',
-                      style:
-                          TextStyle(fontSize: 15, fontWeight: FontWeight.bold),
-                      textAlign: TextAlign.center,
-                      //style: TextStyle(color: Colors.black)
-                    ),
-                  ),
-                  onLongPress: () {
-                    print("long press");
-                  },
-                  onTap: () {
-                    print("tap");
-                  },
-                ),
-              ),
-               Container(
-              height: 200,
-                width: 30,
-              ),
-
-                Container(
-               height: 200,
-                width: 250,
-                child: ListTile(
-                  
-                  title: Image.asset('images/icu.jpg',
-                      height: 170, width: 250, fit: BoxFit.fill),
-                  subtitle: Container(
-                    child: Text(
-                      'ICU',
-                      style:
-                          TextStyle(fontSize: 15, fontWeight: FontWeight.bold),
-                      textAlign: TextAlign.center,
-                      //style: TextStyle(color: Colors.black)
-                    ),
-                  ),
-                  onLongPress: () {
-                    print("long press");
-                  },
-                  onTap: () {
-                    print("tap");
-                  },
-                ),
-              ),
-               Container(
-            height: 200,
-                width: 30,
-              ),
-
-  Container(
-              height: 200,
-                width: 250,
-                child: ListTile(
-                  title: Image.asset('images/dialysis.jpg',
-                    height: 170, width: 250, fit: BoxFit.fill),
-                  subtitle: Container(
-                    child: Text(
-                      'dialysis',
-                      style:
-                          TextStyle(fontSize: 15, fontWeight: FontWeight.bold),
-                      textAlign: TextAlign.center,
-                      //style: TextStyle(color: Colors.black)
-                    ),
-                  ),
-                  onLongPress: () {
-                    print("long press");
-                  },
-                  onTap: () {
-                    print("tap");
-                  },
-                ),
-              ),
-
-
-              //
-            ],
-          ),
-        ),
-*/
-        Container(
-          height: 400,
+           height: double.maxFinite,
+           width: double.maxFinite,
           child: GridView(
             gridDelegate:
-                SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 3),
+                SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 3), 
             children: <Widget>[
-              InkWell(
+          InkWell(
                 child: GridTile(
-                  child: Image.asset('images/done.jpg'),
+                  child:Image.asset('images/done.jpg'),
                   footer: Container(
-                    color: Colors.black.withOpacity(0.5),
+                    color: Colors.black.withOpacity(0.0005),
                     child: Text(
                       'Done operation',
                       style: TextStyle(
@@ -270,16 +61,18 @@ class HomeState extends State<Departments> {
                       textAlign: TextAlign.center,
                     ),
                   ),
+                  
                 ),
                 onTap: () {
                   print("tap");
                 },
               ),
+              
               InkWell(
                 child: GridTile(
                   child: Image.asset('images/beds.jpg'),
                   footer: Container(
-                    color: Colors.black.withOpacity(0.5),
+                    color: Colors.black.withOpacity(0.0005),
                     child: Text(
                       'Avilable beds ',
                       style: TextStyle(
@@ -296,7 +89,7 @@ class HomeState extends State<Departments> {
                 child: GridTile(
                   child: Image.asset('images/ct.jpg'),
                   footer: Container(
-                    color: Colors.black.withOpacity(0.5),
+                    color: Colors.black.withOpacity(0.0005),
                     child: Text(
                       'Problem in CT',
                       style: TextStyle(
@@ -313,7 +106,7 @@ class HomeState extends State<Departments> {
                 child: GridTile(
                   child: Image.asset('images/mri.jpg'),
                   footer: Container(
-                    color: Colors.black.withOpacity(0.5),
+                    color: Colors.black.withOpacity(0.0005),
                     child: Text(
                       'Problem in CT',
                       style: TextStyle(
@@ -330,7 +123,7 @@ class HomeState extends State<Departments> {
                 child: GridTile(
                   child: Image.asset('images/ct.jpg'),
                   footer: Container(
-                    color: Colors.black.withOpacity(0.5),
+                    color: Colors.black.withOpacity(0.0005),
                     child: Text(
                       'Problem in CT',
                       style: TextStyle(
@@ -347,7 +140,7 @@ class HomeState extends State<Departments> {
                 child: GridTile(
                   child: Image.asset('images/ct.jpg'),
                   footer: Container(
-                    color: Colors.black.withOpacity(0.5),
+                    color: Colors.black.withOpacity(0.0005),
                     child: Text(
                       'Problem in CT',
                       style: TextStyle(
@@ -364,7 +157,7 @@ class HomeState extends State<Departments> {
                 child: GridTile(
                   child: Image.asset('images/ct.jpg'),
                   footer: Container(
-                    color: Colors.black.withOpacity(0.5),
+                    color: Colors.black.withOpacity(0.0005),
                     child: Text(
                       'Problem in CT',
                       style: TextStyle(
@@ -381,7 +174,7 @@ class HomeState extends State<Departments> {
                 child: GridTile(
                   child: Image.asset('images/ct.jpg'),
                   footer: Container(
-                    color: Colors.black.withOpacity(0.5),
+                    color: Colors.black.withOpacity(0.0005),
                     child: Text(
                       'Problem in CT',
                       style: TextStyle(
@@ -398,7 +191,7 @@ class HomeState extends State<Departments> {
                 child: GridTile(
                   child: Image.asset('images/ct.jpg'),
                   footer: Container(
-                    color: Colors.black.withOpacity(0.5),
+                    color: Colors.black.withOpacity(0.0005),
                     child: Text(
                       'party with children ',
                       style: TextStyle(
@@ -415,37 +208,10 @@ class HomeState extends State<Departments> {
           ),
         ),
       ]),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
-/*
-class DataSearch extends SearchDelegate<String> {
-  @override
-  List<Widget> buildActions(BuildContext context) {
-    //action for bar
-    return [IconButton(icon: Icon(Icons.clear), onPressed: () {})];
-  }
-
-  @override
-  Widget buildLeading(BuildContext context) {
-    //icon leading
-    return IconButton(
-        icon: Icon(Icons.arrow_back),
-        onPressed: () {
-          close(context, null);
-        });
-  }
-
-  @override
-  Widget buildResults(BuildContext context) {
-    //result
-    return null;
-  }
-
-  @override
-  Widget buildSuggestions(BuildContext context) {
-    //show when person search
-    return Text("data");
-  }
-}
-*/
--- lib/screens/main/components/side_menu.dart
+++ lib/screens/main/components/side_menu.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
-import 'package:admin/screens/dashboard/components/departments.dart';
+//import 'package:admin/screens/dashboard/components/departments.dart';
 
 class SideMenu extends StatelessWidget {
   const SideMenu({
